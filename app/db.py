64f6ab6485194a730cbc1417fd4f1038b12a7fe2
Karo Rios <hkrios10@ucatolica.edu.co>
2025-12-03 10:10:04 -0500

    despliegue Clever Cloud

--- app/db.py
+++ app/db.py
@@ -1,18 +1,31 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
 from typing import Annotated
 from fastapi import Depends
 
-DATABASE_URL = "postgresql+psycopg2://u5hyh5mb6dtuaxyhpy9c:DF57wKULBw1qJxyUZ1hwoWraANL6A6@buc5spn0v3jtzw9pjmkx-postgresql.services.clever-cloud.com:50013/buc5spn0v3jtzw9pjmkx"
+DATABASE_URL = os.getenv("POSTGRESQL_ADDON_URI")
 
-engine = create_engine(DATABASE_URL, echo=True)
+if not DATABASE_URL:
+    DATABASE_URL = os.getenv("SUPABASE_DB_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./local.db"
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://")
+
+engine = create_engine(DATABASE_URL, echo=False)
+
 
 def crear_tablas():
     print("Creando tablas en la base de datos...")
     SQLModel.metadata.create_all(engine)
-    print("Tablas listas")
+    print("Tablas listas ✔")
+
 
 def obtener_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(obtener_session)]
