e35f9c4bc6fed26dbed620e1386ad8a85e0e3136
Karo Rios <hkrios10@ucatolica.edu.co>
2025-12-03 11:58:33 -0500

    actualizacion requirements

--- app/db.py
+++ app/db.py
@@ -3,13 +3,18 @@ from sqlmodel import SQLModel, create_engine, Session
 from typing import Annotated
 from fastapi import Depends
 
+from dotenv import load_dotenv  # <--- IMPORTANTE
+
+# Cargar variables del archivo .env
+load_dotenv()
+
 DATABASE_URL = os.getenv("POSTGRESQL_ADDON_URI")
 
-# Si no existe, intenta usar SUPABASE
-if not DATABASE_URL:
-    DATABASE_URL = os.getenv("POSTGRESQL_ADDON_URI")
+print("🔍 DATABASE_URL ->", DATABASE_URL)
 
-    print("🔍 DATABASE_URL ->", DATABASE_URL)
+# Si no existe Clever, intenta usar Supabase
+if not DATABASE_URL:
+    DATABASE_URL = os.getenv("SUPABASE_DB_URL")
 
 # Si tampoco existe, usa sqlite local
 if not DATABASE_URL:
@@ -21,10 +26,12 @@ if DATABASE_URL.startswith("postgres://"):
 
 engine = create_engine(DATABASE_URL, echo=False)
 
+
 def crear_tablas():
     print("Creando tablas en la base de datos...")
     SQLModel.metadata.create_all(engine)
     print("Tablas listas ✔")
+
 
 def obtener_session():
     with Session(engine) as session:
